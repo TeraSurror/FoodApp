98d1a75d349aff6eb1a5496e3124401ba5e19d04
harshshelar22 <harsh.shelar@spit.ac.in>
2020-02-01 19:10:10 +0530

    'Donation'

--- app/src/main/java/com/example/foodapp/DonorFoodDetails.java
+++ app/src/main/java/com/example/foodapp/DonorFoodDetails.java
@@ -1,33 +1,105 @@
 package com.example.foodapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DonorFoodDetails extends AppCompatActivity {
 
-    LinearLayout inputListWrapper;
-    LinearLayout inputItem;
-    Button addItemButton;
+    EditText userName;
+    EditText userContact;
+    EditText foodDesc;
+    EditText userNote;
+
+
+
+    private DatabaseReference mDatabase;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_food_details);
-
-
-        addItemButton = findViewById(R.id.addItem);
+        mDatabase = FirebaseDatabase.getInstance().getReference("donations");
 
     }
 
-    public void onAddItemClick (View view) {
-        inputListWrapper = findViewById(R.id.inputWrapper);
-        inputItem  = findViewById(R.id.inputItem);
+    public void onPostBtnClicked(View view){
 
-        inputListWrapper.addView(inputItem);
+        String url = "https://foodapp-fa08e.firebaseio.com/donations.json";
 
+        //Name
+        userName = findViewById(R.id.userName);
+        final String userNameIn = userName.getText().toString();
+
+        //Contact
+        userContact = findViewById(R.id.userContact);
+        final String userContactIn = userContact.getText().toString();
+
+        //Description
+        foodDesc = findViewById(R.id.foodDesc);
+        final String foodDescIn = foodDesc.getText().toString();
+
+        //Note
+        userNote = findViewById(R.id.userNote);
+        final String userNotIn = userNote.getText().toString();
+
+        String tet = userContactIn+userNameIn+userNotIn;
+        Toast.makeText(getApplicationContext(),tet,Toast.LENGTH_SHORT).show();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", userNameIn);
+                params.put("contact", userContactIn);
+                params.put("desc",foodDescIn);
+                params.put("note",userNotIn);
+
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this); // this = context
+        queue.add(postRequest);
     }
+
 }
--- app/src/main/java/com/example/foodapp/LoginActivity.java
+++ app/src/main/java/com/example/foodapp/LoginActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodapp.Models.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -27,7 +30,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.example.foodapp.Models.User;
 
 public class LoginActivity extends AppCompatActivity {
 
